49ac031de448c839f941b80d3c74c80b2d3ed454
Dmitry Gavrilenko <gavrilaz0076@gmail.com>
2023-04-24 16:23:18 +0300

    zad 2

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,9 +6,14 @@ class TestArrs(unittest.TestCase):
 
     def test_get(self):
         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
-        # self.assertEqual(arrs.get([1, 2, 3], 3, "test"), 3)
+        self.assertEqual(arrs.get([1, 2, 3], -1, "test"), "test")
+        self.assertEqual(arrs.get([1, 2, 3], 8, "test"), "test")
         self.assertEqual(arrs.get([], 0, "test"), "test")
 
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -5, 4), [1, 2, 3, 4])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -1, 4), [4])
+        self.assertEqual(arrs.my_slice([1, 2, 3], 0, -2), [1])
+        self.assertEqual(arrs.my_slice([], 1, 2), [])
